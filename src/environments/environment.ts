7632b6fc43d3ef99162ce4d2110ae9019bf96f81
augustinkolie <augustinkolie54@gmail.com>
2026-06-29 18:21:08 +0000

    feat: connexion avec le backend en production sur Render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: window.location.hostname === 'localhost' 
+    ? 'http://localhost:3000/api' 
+    : 'https://medical-backend-zz1b.onrender.com/api'
 };
